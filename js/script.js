680978bed69f19fa5340debfef57b8dbcea84621
akjeres <akjeres@gmail.com>
2018-03-06 18:29:00 +0200

    retina test #31

--- js/script.js
+++ js/script.js
@@ -71,7 +71,7 @@ $(document).ready(function() {
 	} else $("div.search_options").css("marginLeft", "");
 	if (isRetina() && isApple()) {
 		$(".fa-chevron-right").css("left", "-4px");
-		alert("Test30");
+		alert("Test31");
 		if ($(window).width() < (650 - getScrollbarWidth())) {
 			$(".change_values").css({
     			top: "-45px",
